d6d8254875964b8ea59f2464304ac886eaa07e51
Blake14 <blakemcpherson2@gmail.com>
2023-07-22 13:12:15 -0500

    style: homepage launch page

--- client/src/components/auth/Homepage.js
+++ client/src/components/auth/Homepage.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import { useNavigate } from 'react-router-dom';
 
-const Homepage = () => {
-	return <div></div>;
+const Homepage = (props) => {
+	const navigate = useNavigate();
+	return (
+		<div>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+				}}
+			>
+				<Image src={props.Logo}></Image>
+			</div>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'space-evenly',
+				}}
+			>
+				<Button
+					onClick={() => {
+						navigate('/lineup');
+					}}
+					variant='success'
+				>
+					Launch Demo
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Homepage;
