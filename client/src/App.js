3ba555abd5ce4d184c88d6f2369c0d649c3b002b
Blake14 <blakemcpherson2@gmail.com>
2023-07-22 10:39:05 -0500

    build: basic app.js structure with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './components/auth/Homepage';
 
 const App = () => {
-	return <div>App</div>;
+	const [bgColor, setBgColor] = useState('#33a1fd');
+	document.body.style.backgroundColor = bgColor;
+	document.body.style.color = 'white';
+	return (
+		<Router>
+			<Routes>
+				<Route>
+					<Route path='/' element={<Homepage />} exact />
+				</Route>
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
