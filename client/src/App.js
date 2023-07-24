ea31a913b7a187e2974dc1bc2c02e7031a2d5091
Blake14 <blakemcpherson2@gmail.com>
2023-07-24 16:07:28 -0500

    build: assign worker function and props

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,6 @@ import GameContainer from './components/GameContainer';
 const App = () => {
 	const [bgColor, setBgColor] = useState('#33a1fd');
 	const [headerColor, setHeaderColor] = useState('#4a4e69');
-	document.body.style.backgroundColor = bgColor;
-	document.body.style.color = 'white';
 	const [gameLog, setGameLog] = useState([]);
 	const [playerData, setPlayerData] = useState({
 		currDay: 1,
@@ -42,7 +40,84 @@ const App = () => {
 			},
 		],
 	});
+	document.body.style.backgroundColor = bgColor;
+	document.body.style.color = 'white';
+	const firstNames = ['Alice', 'Bob', 'Charlie', 'Diana', 'Eve', 'Frank'];
+	const lastNames = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Davis'];
+	const dreamSpecialties = [
+		'Lucid Dreaming',
+		'Nightmare Control',
+		'Dream Manipulation',
+		'Dream Healing',
+	];
+	const minAge = 20;
+	const maxAge = 60;
+	const minSkillLevel = 1;
+	const maxSkillLevel = 10;
+	const generateRandomWorker = () => {
+		const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+		const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+		const age = Math.floor(Math.random() * (maxAge - minAge + 1)) + minAge;
+		const dreamSpecialty =
+			dreamSpecialties[Math.floor(Math.random() * dreamSpecialties.length)];
+		const skillLevel =
+			Math.floor(Math.random() * (maxSkillLevel - minSkillLevel + 1)) +
+			minSkillLevel;
 
+		const randomWorker = {
+			firstName,
+			lastName,
+			age,
+			dreamSpecialty,
+			skillLevel,
+		};
+
+		return randomWorker;
+	};
+
+	const handleGenerateWorker = () => {
+		const randomWorker = generateRandomWorker();
+		console.log(randomWorker);
+		setPlayerData((prevState) => {
+			return {
+				...prevState,
+				workers: [...prevState.workers, randomWorker],
+			};
+		});
+		setGameLog((prevState) => {
+			return [
+				...prevState,
+				`Night ${playerData.currDay}: Worker Hired [${randomWorker.firstName} ${randomWorker.lastName}]`,
+			];
+		});
+		updateScroll();
+	};
+	const updateScroll = () => {
+		var element = document.getElementById('gameLogScroll');
+		element.scrollTop = element.scrollHeight;
+	};
+	const UpgradeData = [
+		{
+			level: 1,
+			cost: 0,
+		},
+		{
+			level: 2,
+			cost: 45,
+		},
+		{
+			level: 3,
+			cost: 100,
+		},
+		{
+			level: 4,
+			cost: 200,
+		},
+		{
+			level: 5,
+			cost: 500,
+		},
+	];
 	return (
 		<Router>
 			<Routes>
@@ -52,11 +127,15 @@ const App = () => {
 						path='/lineup'
 						element={
 							<GameContainer
-								bgColor={bgColor}
 								playerData={playerData}
+								setPlayerData={setPlayerData}
+								headerColor={headerColor}
+								handleGenerateWorker={handleGenerateWorker}
+								generateRandomWorker={generateRandomWorker}
+								bgColor={bgColor}
 								gameLog={gameLog}
 								setGameLog={setGameLog}
-								headerColor={headerColor}
+								UpgradeData={UpgradeData}
 							/>
 						}
 					/>
