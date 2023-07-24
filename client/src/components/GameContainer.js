11ad7f51341354acc7dd2b78b9419a06b9daef92
Blake14 <blakemcpherson2@gmail.com>
2023-07-24 16:04:19 -0500

    style: flex styling and props

--- client/src/components/GameContainer.js
+++ client/src/components/GameContainer.js
@@ -2,8 +2,18 @@ import React from 'react';
 import WorkersModule from './game-container/WorkersModule';
 import FactoryFloor from './game-container/FactoryFloor';
 import ResourceBar from './game-container/ResourceBar';
+import GameLog from './game-container/GameLog';
+import UpgradeMenu from './game-container/UpgradeMenu';
 
 const GameContainer = (props) => {
+	if (props.gameLog.length <= 0) {
+		props.setGameLog((prevState) => {
+			return [
+				...prevState,
+				`Night ${props.playerData.currDay}: Game Started ...`,
+			];
+		});
+	}
 	return (
 		<div
 			style={{
@@ -21,11 +31,26 @@ const GameContainer = (props) => {
 				<WorkersModule
 					playerData={props.playerData}
 					headerColor={props.headerColor}
+					handleGenerateWorker={props.handleGenerateWorker}
+					generateRandomWorker={props.generateRandomWorker}
+					setPlayerData={props.setPlayerData}
 				/>
 				<FactoryFloor
 					playerData={props.playerData}
+					setPlayerData={props.setPlayerData}
 					headerColor={props.headerColor}
+					UpgradeData={props.UpgradeData}
 				/>
+				<div
+					style={{
+						width: '25%',
+						marginLeft: 10,
+						marginTop: 10,
+					}}
+				>
+					<UpgradeMenu headerColor={props.headerColor} />
+					<GameLog gameLog={props.gameLog} headerColor={props.headerColor} />
+				</div>
 			</div>
 		</div>
 	);
