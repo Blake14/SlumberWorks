2a33616ba47d1c63103a3e352c997614221efae7
Blake14 <blakemcpherson2@gmail.com>
2023-07-25 11:35:04 -0500

    style: trash icon for deleting slot

--- client/src/components/game-container/BuildingSlot.js
+++ client/src/components/game-container/BuildingSlot.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsTrash } from 'react-icons/bs';
 
 const BuildingSlot = (props) => {
 	const handleMouseEnter = (index) => {
@@ -12,47 +13,126 @@ const BuildingSlot = (props) => {
 		updatedSlots[index].isHover = false;
 		props.setSlots(updatedSlots);
 	};
-	return (
-		<div
-			key={props.slot.id}
-			onMouseEnter={() => handleMouseEnter(props.index)}
-			onMouseLeave={() => handleMouseLeave(props.index)}
-			style={{
-				backgroundColor: props.slot.isHover ? props.bgColor : 'transparent',
-				width: props.slot.width,
-				height: props.slot.height,
-				position: 'absolute',
-				top: props.slot.posY,
-				left: props.slot.posX,
-				border: `4px dotted ${
-					props.slot.isHover && 1 === 2 //force false
-						? 'transparent'
-						: props.bgColor
-				}`,
-				borderRadius: 15,
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				color: props.slot.isHover ? 'white' : props.bgColor,
-				fontSize: props.slot.isHover ? 15 : 13,
-				cursor: 'pointer',
-			}}
-		>
-			<p
+	if (props.slot.building === 'None') {
+		return (
+			<div
+				key={props.slot.id}
+				onMouseEnter={() => handleMouseEnter(props.index)}
+				onMouseLeave={() => handleMouseLeave(props.index)}
 				style={{
-					width: '100%',
-					textAlign: 'center',
-					marginTop: 15,
+					backgroundColor: props.slot.isHover ? props.bgColor : 'transparent',
+					width: props.slot.width,
+					height: props.slot.height,
+					position: 'absolute',
+					top: props.slot.posY,
+					left: props.slot.posX,
+					border: `4px dotted ${
+						props.slot.isHover && 1 === 2 //force false
+							? 'transparent'
+							: props.bgColor
+					}`,
+					borderRadius: 15,
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					color: props.slot.isHover ? 'white' : props.bgColor,
+					fontSize: props.slot.isHover ? 15 : 13,
+					cursor: 'pointer',
+					fontVariant: 'all-petite-caps',
 				}}
 			>
-				<strong>
-					{props.slot.building === 'None'
-						? 'Add Building'
-						: props.slot.building}
-				</strong>
-			</p>
-		</div>
-	);
+				<p
+					style={{
+						width: '100%',
+						textAlign: 'center',
+						marginTop: 15,
+					}}
+				>
+					<strong>Add Building</strong>
+				</p>
+			</div>
+		);
+	} else {
+		return (
+			<div
+				key={props.slot.id}
+				onMouseEnter={() => handleMouseEnter(props.index)}
+				onMouseLeave={() => handleMouseLeave(props.index)}
+				style={{
+					backgroundColor: props.bgColor,
+					width: props.slot.width,
+					height: props.slot.height,
+					position: 'absolute',
+					top: props.slot.posY,
+					left: props.slot.posX,
+					border: `2px solid black`,
+					borderRadius: 15,
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					color: 'white',
+					fontSize: props.slot.isHover ? 15 : 13,
+					cursor: 'pointer',
+					fontVariant: 'all-petite-caps',
+				}}
+			>
+				<div
+					style={{
+						position: 'absolute',
+						top: -20,
+						left: -20,
+						fontSize: 16,
+						color: props.slot.isHover ? 'black' : 'transparent',
+					}}
+				>
+					<BsTrash />
+				</div>
+				<div
+					style={{
+						position: 'absolute',
+						top: 2,
+					}}
+				>
+					<p>
+						<strong>{props.slot.building}</strong>
+					</p>
+				</div>
+				<div
+					style={{
+						position: 'absolute',
+						top: 18,
+						fontSize: 10,
+					}}
+				>
+					<p>
+						<strong>Status: </strong>Idle
+					</p>
+				</div>
+				<div
+					style={{
+						position: 'absolute',
+						top: 28,
+						fontSize: 10,
+					}}
+				>
+					<p>
+						<strong>Workers: </strong>0
+					</p>
+				</div>
+				<div
+					style={{
+						position: 'absolute',
+						top: 38,
+						fontSize: 10,
+					}}
+				>
+					<p>
+						<strong>Manager: </strong>Jesus Christ
+					</p>
+				</div>
+			</div>
+		);
+	}
 };
 
 export default BuildingSlot;
